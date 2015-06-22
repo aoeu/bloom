fbc5de99541bdbc51cbb3f04992238837d0f9ccc
aoeu <travis@mcdem.us>
2015-06-21 21:29:32 -0400

    Add test for might contains function

--- bloom_test.go
+++ bloom_test.go
@@ -4,13 +4,21 @@ import (
 	"testing"
 )
 
-
-func TestPut(t  *testing.T) {
-	bf, err := New(5)
-	if err != nil {
-		t.Errorf("%+v", err)
-	}
+func TestPut(t *testing.T) {
+	bf, _ := New(5)
 	if err := bf.Put([]byte("foo")); err != nil {
 		t.Errorf("%+v", err)
+	}
+}
+
+func TestMightContain(t *testing.T) {
+	bf, _ := New(1)
+	key := []byte("foo")
+	if actual := bf.MightContain(key); actual != false {
+		t.Errorf("Expected false but received %v", actual)
+	}
+	bf.Put(key)
+	if actual := bf.MightContain(key); actual != true {
+		t.Errorf("Expected true but received %v", actual)
 	}
 }
